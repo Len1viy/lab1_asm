4f6c7e195c3f816ad9e32fa598015eeb5d327f64
Vadim <vadim.nevara@gmail.com>
2024-03-05 22:12:54 +0300

    Second solution with flags

--- lab.s
+++ lab.s
@@ -4,23 +4,15 @@ section .data
 res:
 	dq	0
 a:
-	dq	100
+	dq	1000
 b:
-	dd	20
+	dd	200
 c:
-	dd	80
+	dd	320	
 d:
-	db	40
+	db	20
 e:
-	dw	10
-proizv1:
-	dq	0
-proizv2:
-	dq	0
-div1:
-	dq	0
-div2:
-	dq	0
+	dw	100
 ;16
 section .text
 global _start
@@ -28,27 +20,52 @@ _start:
 	mov	ecx, dword[c]
 	movzx	edi, byte[d]
 	movzx	esi, word[e]
+	cmp	edi, 0
+	je	zf
 	mov	eax, ecx
+	cdqe
 	mul	edi
+	jc	cf
 	mul	esi
-	mov	dword[proizv2], eax
+	jc	cf
+	mov	r9d, eax
 	mov	rbp, qword[a]
 	mov	ebx, dword[b]
+	cmp	ebx, 0
+	je	zf
 	mov	rax, rbp
 	mul	ebx
+	jc	cf
 	mul	ecx
-	mov	qword[proizv1], rax
+	jc	cf
+	mov	r8, rax
 	mov	rax, rbp
 	div	ebx
-	mov	qword[div1], rax
+	
+	mov	r10, rax
 	mov	eax, ecx
+	cdqe
+	mov	rdx, 0
 	div	edi
-	add	eax, dword[div1]
-	mov	esp, eax
-	mov	rax, qword[proizv1]
-	sub	rax, qword[proizv2]
-	div	esp
+	add	rax, r10
+	cmp	rax, 0
+	je	zf
+	mov	rsp, rax
+	mov	rax, r8
+	sub	rax, r9
+	jc	cf
+	div	rsp
 	mov	qword[res], rax
 	mov	eax, 60
 	mov	edi, 0
+	syscall
+
+zf:
+	mov eax, 60
+	mov edi, 1
+	syscall
+
+cf:
+	mov eax, 60
+	mov	edi, 2
 	syscall
